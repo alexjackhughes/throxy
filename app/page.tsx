4554de031de0ef3bf8e1195b1b133721621d9989
Alex <alex@alexjackhughes.com>
2025-06-18 20:20:11 +0100

    Add favicon and tab title

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Throxy",
+};
+
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
 }
