b0ba474b4b55bd1fc9811109b4630135363cbf0a
Alex <alex@alexjackhughes.com>
2025-06-18 20:10:56 +0100

    Add company model

---
+++ models/company.ts
@@ -0,0 +1,36 @@
+export interface Company {
+  company_name: string;
+  domain: string;
+  city?: string;
+  country: string;
+  employee_size: EmployeeSize;
+  id?: string; // remove this later, just for my memory on what goes into table
+  raw_json?: string; // remove this later, just for my memory goes into table
+}
+
+export type EmployeeSize =
+  | "1‑10"
+  | "11‑50"
+  | "51‑200"
+  | "201‑500"
+  | "501‑1 000"
+  | "1 001‑5 000"
+  | "5 001‑10 000"
+  | "10 000+";
+
+export type Companies = Company[];
+
+export const defaultCompany: Company = {
+  company_name: "Apple",
+  domain: "apple.com",
+  country: "United States",
+  employee_size: "1‑10",
+  id: "1",
+  raw_json: JSON.stringify({
+    company_name: "Apple",
+    domain: "apple.com",
+    country: "United States",
+  }),
+};
+
+export const defaultCompanies: Companies = [defaultCompany];
